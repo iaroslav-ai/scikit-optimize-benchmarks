9f6d4f037f10eb4e9b5fb6b85bbaf48077131089
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2017-08-12 17:53:01 +0200

    Update order to fix the installation of GPy

--- install_software.sh
+++ install_software.sh
@@ -7,10 +7,7 @@
 # install necessary dependencies if not present
 apt-get update
 apt-get install python python3 python-pip python3-pip git -y
-
-# install dask for distributed computing
-pip install dask[complete]
-pip3 install dask[complete]
+apt-get install python-tk python3-tk -y # necessary for Docker image
 
 # install necessary python packages
 pip install -r requirements.txt
@@ -20,28 +17,35 @@ pip3 install -r requirements.txt
 pip install hyperopt
 pip3 install hyperopt
 
-# install spearmint
-cd
-git clone https://github.com/HIPS/Spearmint.git
-cd Spearmint
-pip install -e .
-cd
-
-# install SMAC3
-apt-get install python3-tk
-apt-get install swig
-pip3 install https://github.com/automl/SMAC3/archive/master.zip
-
 # install GPyOpt
 pip install Gpy
 pip3 install Gpy
 pip install GpyOpt
 pip3 install GpyOpt
 
+# install dask for distributed computing
+pip install dask[complete]
+pip3 install dask[complete]
+
 # install mongodb (necessary for spearmint)
 pip install pymongo
 bash mongod_install.sh
 service mongod start
+the_script_path=$PWD
+
+# install spearmint
+cd
+git clone https://github.com/HIPS/Spearmint.git
+cd Spearmint
+pip install -e .
+cd $the_script_path
+
+# install SMAC3
+apt-get install swig
+pip3 install https://github.com/automl/SMAC3/archive/master.zip
+
+
+
 
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "Please run 'sudo service mongod start' in terminal before using 'spearmint_minimize'"
